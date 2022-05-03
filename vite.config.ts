66f3f1a2051f399a2e7e1327b9d2d7b38d3b2b7a
doraemon-daffodils <81766259+doraemon-daffodils@users.noreply.github.com>
2022-05-03 20:00:48 +0530

    dep

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Markdown from 'vite-plugin-md'
 import WindiCSS from 'vite-plugin-windicss'
+import { VitePWA } from 'vite-plugin-pwa'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import Inspect from 'vite-plugin-inspect'
 import Prism from 'markdown-it-prism'
@@ -29,6 +30,8 @@ export default defineConfig({
     },
   },
   plugins: [
+    VitePWA(),
+    
     Vue({
       include: [/\.vue$/, /\.md$/],
     }),
